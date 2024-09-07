199d5e19a17d451509924dbcdbf4980c83174ae7
Anjani Nandan <116096372+anjanicoder@users.noreply.github.com>
2024-09-07 20:19:28 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,4 @@
-import streamlit as st
+shrimport streamlit as st
 import matplotlib.pyplot as plt
 import plotly.express as px
 import sqlalchemy
@@ -204,7 +204,7 @@ with st.sidebar:
             st.write("Made with Gemini pro ")
 
 
-        st.write("<div>Developed by - <span style=\"color: cyan; font-size: 24px; font-weight: 600;\">Jaishree Yadavn</span></div>",unsafe_allow_html=True)
+        st.write("<div>Developed by - <span style=\"color: cyan; font-size: 24px; font-weight: 600;\">Jaishree Yadav</span></div>",unsafe_allow_html=True)
 
 
 # Define a function to format numbers
